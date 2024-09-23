a1664a95c03de03e71794dbddb39330f4282c2f6
Andy C <85andyc@gmail.com>
2024-09-23 16:40:38 -0400

    Listing Screen XML
    First commit of the listing screen XML

---
+++ app/src/main/java/com/example/bookru/TimePickerDialogue.kt
@@ -0,0 +1,22 @@
+package com.example.bookru
+
+class TimePickerDialogue {
+    // basically this is the code we will have to use https://stackoverflow.com/questions/11837581/how-to-make-the-timepicker-smaller
+    // make it a dialogue instead of have it natively
+    /*Button setTime;
+    setTime = (Button)findViewById(R.id.button1);
+    setTime.setOnClickListener(this);
+
+    public void onClick(View v)
+    {
+        TimePickerDialog tp1 = new TimePickerDialog(this, this, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true);
+        tp1.show();
+    }
+
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute)
+    {
+        cal.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        cal.set(Calendar.MINUTE, minute);
+        //rest of the code
+    }*/
+}
